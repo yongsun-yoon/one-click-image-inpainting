9de8bceb568af25468a8740801d6974b13079825
yongsun <respect5716@gmail.com>
2023-01-30 11:36:30 +0900

    update

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import cv2
 import torch
 import numpy as np
 import streamlit as st
+from io import BytesIO
 from PIL import Image
 from streamlit.runtime.scriptrunner import get_script_run_ctx
 
@@ -36,17 +37,17 @@ def get_inpaint_config(**kwargs):
 def load_models(device='cpu'):
     segment_processor = AutoProcessor.from_pretrained("shi-labs/oneformer_ade20k_swin_tiny")
     segment_model = AutoModelForUniversalSegmentation.from_pretrained("shi-labs/oneformer_ade20k_swin_tiny")
-    _ = segment_model.eval().requires_grad_(False)
+    _ = segment_model.eval().requires_grad_(False).to(device)
 
     inpaint_config = get_inpaint_config()
     inpaint_model = ModelManager('lama', device=device)
     return segment_processor, segment_model, inpaint_config, inpaint_model
 
-@st.cache
-def run_segment(np_image):
-    segment_inputs = segment_processor(images=np_image, task_inputs=["panoptic"], return_tensors="pt").to(device)
+
+def run_segment(pil_image):
+    segment_inputs = segment_processor(images=pil_image, task_inputs=["panoptic"], return_tensors="pt").to(device)
     segment_outputs = segment_model(**segment_inputs)
-    segment_outputs = segment_processor.post_process_panoptic_segmentation(segment_outputs, target_sizes=[np_image.shape[:2]])[0]
+    segment_outputs = segment_processor.post_process_panoptic_segmentation(segment_outputs, target_sizes=[pil_image.size[::-1]])[0]
     segmentation = segment_outputs['segmentation']
     return segmentation
 
@@ -64,22 +65,47 @@ def main():
     session_dir = f'results/{session_id}'
     os.makedirs(session_dir, exist_ok=True)
 
-    img_file = st.file_uploader(label='얼굴 사진 업로드', type=['png', 'jpg', 'jpeg'])
+    original_path = f'{session_dir}/original.png'
+    inpainted_path = f'{session_dir}/inpainted.png'
+
+    img_file = st.file_uploader(label='Upload image', type=['png', 'jpg', 'jpeg'])
     if img_file is not None:
         pil_image = Image.open(img_file).convert('RGB')
+        pil_image.save(original_path)
+        segmentation = run_segment(pil_image)
+    
+    if os.path.exists(original_path) or os.path.exists(inpainted_path):
+        st.write('Input')
+        if os.path.exists(inpainted_path):
+            pil_image = Image.open(inpainted_path)
+        else:
+            pil_image = Image.open(original_path)
         np_image = np.array(pil_image)
-        segmentation = run_segment(np_image)
-
         value = streamlit_image_coordinates(pil_image)
-
+            
         if value is not None:
             instance_id = segmentation[value['y'], value['x']].item()
             mask = torch.where(segmentation==instance_id, 1, 0)
-            mask = mask.numpy() * 255.
+            mask = mask.cpu().numpy() * 255.
             
             inpainted_image = run_inpaint(np_image, mask)
             inpainted_image = Image.fromarray(inpainted_image)
+            inpainted_image.save(inpainted_path)
+            
+            st.write('Output')
             st.image(inpainted_image)
+
+            buf = BytesIO()
+            inpainted_image.save(buf, format="JPEG")
+            byte_im = buf.getvalue()
+            btn = st.download_button(
+                label="Download Image",
+                data=byte_im,
+                file_name="image.png",
+                mime="image/jpeg",
+            )
+
+
 
 if __name__ == '__main__':
     device = args.device
